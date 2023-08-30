1994b7e68ba737837995e6caf3e9845f37e4bb7d
Francisco <francisco@mail.com>
2023-08-30 10:48:29 -0400

    Se agrega función para mostrar descripcion de error

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,17 @@
 import tkinter
 from tkinter import ttk
+import json
+
+with open('errores.json','r') as archivo:
+    datos = json.load(archivo)
+
+def obtenerDesc():
+    dato_ingresado = entrada_texto.get()
+    for error in datos['errores']:
+        if error['codigo'] == dato_ingresado:
+            desc_error.config(text=error['desc'])
+            break
+        
 
 ventana = tkinter.Tk()
 ventana.title("Codigos de error")
@@ -11,7 +23,7 @@ titulo.pack(pady= 10, padx=10)
 entrada_texto = ttk.Entry(ventana)
 entrada_texto.pack(pady=10,padx=10)
 
-btn_buscar = ttk.Button(ventana, text="Buscar codigo")
+btn_buscar = ttk.Button(ventana, text="Buscar codigo",command=obtenerDesc)
 btn_buscar.pack(pady=10, padx=10)
 
 desc_error = ttk.Label(ventana, text="")
